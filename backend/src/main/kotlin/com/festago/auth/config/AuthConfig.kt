509d3e46ddfdbc50390cc350e624da417439220f
seokjin8678 <seokjin8678@gmail.com>
2024-10-24 02:58:26 +0900

    feat: 로그아웃 API 요청 시 토큰이 없거나 만료되어도 200 응답이 반환되도록 수정

--- backend/src/main/kotlin/com/festago/auth/config/AuthConfig.kt
+++ backend/src/main/kotlin/com/festago/auth/config/AuthConfig.kt
@@ -1,16 +1,92 @@
 package com.festago.auth.config
 
+import com.festago.auth.annotation.Authorization
+import com.festago.auth.domain.Role
+import com.festago.auth.domain.authentication.AuthenticateContext
 import com.festago.auth.domain.oauth2.OAuth2Client
 import com.festago.auth.domain.oauth2.OAuth2Clients
 import com.festago.auth.domain.openid.OpenIdClient
 import com.festago.auth.domain.openid.OpenIdClients
+import com.festago.auth.domain.token.http.CompositeHttpRequestTokenExtractor
+import com.festago.auth.domain.token.http.CookieHttpRequestTokenExtractor
+import com.festago.auth.domain.token.http.HeaderHttpRequestTokenExtractor
+import com.festago.auth.domain.token.jwt.AuthenticationTokenExtractor
+import com.festago.auth.web.argument.AdminAuthenticationArgumentResolver
+import com.festago.auth.web.argument.AuthenticationArgumentResolver
+import com.festago.auth.web.argument.MemberAuthenticationArgumentResolver
+import com.festago.auth.web.interceptor.AnnotationAuthorizationInterceptor
+import com.festago.auth.web.interceptor.FixedAuthorizationInterceptor
+import com.festago.common.interceptor.AnnotationDelegateInterceptor
+import com.festago.common.interceptor.HttpMethodDelegateInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-private class AuthConfig {
+private class AuthConfig(
+    private val adminAuthenticationTokenExtractor: AuthenticationTokenExtractor,
+    private val compositeAuthenticationTokenExtractor: AuthenticationTokenExtractor,
+    private val authenticateContext: AuthenticateContext,
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(MemberAuthenticationArgumentResolver(authenticateContext))
+        resolvers.add(AdminAuthenticationArgumentResolver(authenticateContext))
+        resolvers.add(AuthenticationArgumentResolver(authenticateContext))
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(
+            HttpMethodDelegateInterceptor.builder()
+                .allowMethod(HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.PATCH)
+                .interceptor(adminFixedAuthorizationInterceptor())
+                .build()
+        )
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/admin/api/v1/auth/login", "/admin/api/v1/auth/initialize")
+
+        registry.addInterceptor(
+            AnnotationDelegateInterceptor.builder()
+                .annotation(Authorization::class.java)
+                .interceptor(annotationAuthorizationInterceptor())
+                .build()
+        )
+            .addPathPatterns("/api/**")
+    }
+
+    @Bean
+    fun adminFixedAuthorizationInterceptor(): FixedAuthorizationInterceptor {
+        return FixedAuthorizationInterceptor(
+            compositeHttpRequestTokenExtractor(),
+            adminAuthenticationTokenExtractor,
+            authenticateContext,
+            Role.ADMIN
+        )
+    }
+
+    @Bean
+    fun annotationAuthorizationInterceptor(): AnnotationAuthorizationInterceptor {
+        return AnnotationAuthorizationInterceptor(
+            compositeHttpRequestTokenExtractor(),
+            compositeAuthenticationTokenExtractor,
+            authenticateContext
+        )
+    }
+
+    @Bean
+    fun compositeHttpRequestTokenExtractor(): CompositeHttpRequestTokenExtractor {
+        return CompositeHttpRequestTokenExtractor(
+            listOf(
+                HeaderHttpRequestTokenExtractor(),
+                CookieHttpRequestTokenExtractor()
+            )
+        )
+    }
 
     @Bean
     fun oAuth2Clients(oAuth2Clients: List<OAuth2Client>): OAuth2Clients {
